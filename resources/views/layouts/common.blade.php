677c4cb551ff07e59a68e15f6c39b118abd390f7
kotareaux <lcak@live.jp>
2021-08-28 17:02:35 +0900

    viewport

--- resources/views/layouts/common.blade.php
+++ resources/views/layouts/common.blade.php
@@ -2,7 +2,7 @@
 <html lang="ja">
 <head>
 <!--meta name="viewport" content="width=device-width,initial-scale=0.5"-->
-<meta name="viewport" content="width=device-width,initial-scale=1,viewport-fit=cover">
+<meta name="viewport" content="width=device-width,initial-scale=2,viewport-fit=cover">
 <link  href="style.css" rel="stylesheet" type="text/css">
 <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@400;700&display=swap" rel="stylesheet">
 
