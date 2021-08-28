5e2dffda1325e52d74e495b415cbecf8ebe3a940
kotareaux <lcak@live.jp>
2021-08-28 15:05:03 +0900

    viewport

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,4 +1,4 @@
 <header>
     <span class="nama">アメミュ練習室予約システム</span>
-    <!-- span class="slbtn"><a href="config/login.php"><button class='yhhb'>Login</button></a></span -->
+    <span class="slbtn"><a href="config/login.php"><button class='yhhb'>Login</button></a></span>
 </header>
