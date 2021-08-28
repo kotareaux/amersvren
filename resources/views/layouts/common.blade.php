ccfccbc5ac8d74738f7ceb2bcad41a3bc538d7b0
kotareaux <lcak@live.jp>
2021-08-28 16:56:30 +0900

    css

--- resources/views/layouts/common.blade.php
+++ resources/views/layouts/common.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="ja">
 <head>
-<Meta name="viewport" content="width=device-width,initial-scale=1.0">
+<meta name="viewport" content="width=device-width,initial-scale=0.5">
 <link  href="style.css" rel="stylesheet" type="text/css">
 <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@400;700&display=swap" rel="stylesheet">
 
